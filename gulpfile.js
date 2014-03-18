d3c35e1233b246ae386ee42869ff13fd52416a10
Ben Drucker <bvdrucker@gmail.com>
2014-03-18 09:29:35 -0400

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var files = {
+  src: 'src/*.js',
+  test: 'test/**/*.js'
+};
+
+files.all = [files.src, files.test];
+
+gulp.task('lint', function () {
+  return gulp.src(files.all)
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.jshint.reporter('fail'));
+});
+
+gulp.task('test', ['lint'], function (done) {
+  gulp.src(files.src)
+    .pipe(plugins.istanbul())
+    .on('end', function () {
+      gulp.src(files.test)
+        .pipe(plugins.mocha())
+        .pipe(plugins.istanbul.writeReports())
+        .on('end', done);
+    });
+})

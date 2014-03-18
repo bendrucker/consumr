22c5ef38f9f9d68881add2723622839bbfac3345
Ben Drucker <bvdrucker@gmail.com>
2014-03-18 11:44:57 -0400

    Test watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 'use strict';
 
 var gulp    = require('gulp');
+var gutil   = require('gulp-util');
 var plugins = require('gulp-load-plugins')();
+
+var logIfWatch = function (err) {
+  if (watch) {
+    gutil.log(err.toString());
+    this.emit('end');
+  } else {
+    throw err;
+  }
+};
 
 var files = {
   src: 'src/*.js',
@@ -14,7 +24,8 @@ gulp.task('lint', function () {
   return gulp.src(files.all)
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'))
-    .pipe(plugins.jshint.reporter('fail'));
+    .pipe(plugins.jshint.reporter('fail'))
+    .on('error', logIfWatch);
 });
 
 gulp.task('test', ['lint'], function (done) {
@@ -24,7 +35,14 @@ gulp.task('test', ['lint'], function (done) {
     .on('end', function () {
       gulp.src(files.test)
         .pipe(plugins.mocha())
+        .on('error', logIfWatch)
         .pipe(plugins.istanbul.writeReports())
         .on('end', done);
     });
+});
+
+var watch;
+gulp.task('watch', function () {
+  watch = true;
+  return gulp.watch(files.all, ['test']);
 });

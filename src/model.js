e82a83e5a1ad029535102533f0ae46ebdcd16e5c
Ben Drucker <bvdrucker@gmail.com>
2014-03-18 10:56:00 -0400

    Model URLs

--- src/model.js
+++ src/model.js
@@ -1,10 +1,19 @@
 'use strict';
 
 var needle = require('needle');
+var url    = require('url');
 var extend = require('extend');
 
 var Model = function (attributes) {
   extend(this, attributes);
+};
+
+Model.prototype.isNew = function () {
+  return (typeof this.id === 'undefined' || this.id === null);
+};
+
+Model.prototype.url = function () {
+  return this.base + '/' + this.path + (this.isNew() ? '' : '/' + this.id); 
 };
 
 module.exports = Model;
--- test/unit/model.js
+++ test/unit/model.js
@@ -4,10 +4,33 @@ var Model = require('../../src/model');
 
 describe('Model', function () {
 
+  var model;
+  beforeEach(function () {
+    model = new Model();
+  });
+
   describe('Constructor', function () {
 
     it('sets up attributes', function () {
       expect(new Model({foo: 'bar'})).to.have.property('foo', 'bar');
+    });
+
+  });
+
+  describe('#url', function () {
+
+    beforeEach(function () {
+      model.base = 'http://base';
+      model.path = 'model';
+    });
+
+    it('is the collection endpoint for new models', function () {
+      expect(model.url()).to.equal('http://base/model');
+    });
+
+    it('is the model endpoint for persisted models', function () {
+      model.id = 0;
+      expect(model.url()).to.equal('http://base/model/0');
     });
 
   });

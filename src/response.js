a51707858276e1b9e1873ba7415935201b6c490d
Ben Drucker <bvdrucker@gmail.com>
2014-03-20 12:59:23 -0400

    WIP response

---
+++ src/response.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var pick        = require('lodash.pick');
+var httpStatus  = require('http-status');
+var createError = require('create-error');
+
+var internals = {};
+
+internals.error = function (response) {
+  return new exports.ResponseError(
+    internals.error.message.call(this, response),
+    internals.error.properties.call(this, response)
+  );
+};
+
+internals.error.message = function (response) {
+  return this.errorProperty ? response.body[this.errorProperty] : httpStatus[response.statusCode];
+};
+
+internals.error.properties = function (response) {
+  return pick(response, 'body', 'statusCode');
+};
+
+exports.ResponseError = createError('ResponseError');
+
+exports.catch = function (response) {
+  if (response.statusCode > 399) throw internals.error.call(this, response);
+  return response;
+};

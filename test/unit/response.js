216f043f925b2d6508a5f1848771936c462d9086
Ben Drucker <bvdrucker@gmail.com>
2014-03-20 13:25:07 -0400

    Unit test response#catch

---
+++ test/unit/response.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var response = require('../../src/response');
+
+describe('Response', function () {
+
+  describe('#catch', function () {
+
+    describe('response.statusCode < 400', function () {
+
+      it('is a pass through', function () {
+        var res = {statusCode: 200};
+        expect(response.catch(res)).to.equal(res);
+      });
+
+    });
+
+
+    describe('response.statusCode > 399', function () {
+
+      var context;
+      beforeEach(function () {
+        context = {};
+      });
+
+      it('throws a ResponseError', function () {
+        expect(response.catch.bind(context, {
+          statusCode: 400
+        })).to.throw(response.ResponseError);
+      });
+
+      it('appends the statusCode to the error', function () {
+        expect(response.catch.bind(context, {
+          statusCode: 400
+        })).to.throw(sinon.match.has('statusCode', 400));
+      });
+
+      it('appends the body to the error', function () {
+        expect(response.catch.bind(context, {
+          statusCode: 400,
+          body: {}
+        })).to.throw(sinon.match.has('body'));
+      });
+
+      describe('errorProperty', function () {
+
+        it('uses extracts the error message based on model.errorProperty', function () {
+          context = {
+            errorProperty: 'error'
+          };
+          expect(response.catch.bind(context, {
+            statusCode: 400,
+            body: {
+              error: 'err'
+            }
+          })).to.throw('err');
+        });
+
+        it('can deep extract using dot notation', function () {
+          context = {
+            errorProperty: 'error.message'
+          };
+          expect(response.catch.bind(context, {
+            statusCode: 400,
+            body: {
+              error: {
+                message: 'err'
+              }
+            }
+          })).to.throw('err');
+        });
+
+      });
+
+      it('falls back to a statusCode dictionary name', function () {
+        expect(response.catch.bind(context, {
+          statusCode: 500,
+        })).to.throw('Internal Server Error');
+      });
+
+    });
+
+  });
+
+});

93ea503fcb205b24c9202d04ade4515e08295d92
Ben Drucker <bvdrucker@gmail.com>
2014-03-20 15:03:46 -0400

    Collection#fetch

--- src/collection.js
+++ src/collection.js
@@ -1,13 +1,23 @@
 'use strict';
 var querystring = require('querystring');
 var Promise     = require('bluebird');
-var Model       = require('./model');
 var needle      = Promise.promisifyAll(require('needle'));
+var response    = require('./response');
 
 var internals = {};
 
 internals.querystring = function () {
   return Object.keys(this.attributes).length? '?' + querystring.stringify(this.attributes) : '';
+};
+
+internals.cast = function (attributes) {
+  return new this(attributes);
+};
+
+internals.find = function (model) {
+  return this.filter(function (model) {
+    return model.id === this.id;
+  }, model)[0];
 };
 
 var Collection = function (attributes) {
@@ -26,13 +36,30 @@ Collection.prototype.reset = function () {
 };
 
 Collection.prototype.fetch = function () {
-  Promise
+  return Promise
     .bind(this)
     .then(function () {
       return this.model.prototype.url() + internals.querystring.call(this);
     })
     .then(needle.getAsync)
-    .then
+    .bind(this.model.prototype)
+    .then(response.parse)
+    .bind(this)
+    .reduce(function (newModels, modelData) {
+      var existing = internals.find.call(this, modelData);
+      if (existing) {
+        existing.set(modelData);
+      } else {
+        newModels.push(modelData);
+      }
+      return newModels;
+    }, [])
+    .bind(this.model)
+    .map(internals.cast)
+    .bind(this)
+    .then(function (models) {
+      this.push.apply(this, models);
+    });
 };
 
 module.exports = Collection;
--- test/unit/collection.js
+++ test/unit/collection.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Collection = require('../../src/collection');
+var ModelBase  = require('../../src/model');
 var needle     = require('needle');
 
 describe('Collection', function () {
@@ -35,6 +36,99 @@ describe('Collection', function () {
       collection.attributes = {foo: 'bar'};
       collection.reset();
       expect(collection.attributes).to.not.have.property('foo');
+    });
+
+  });
+
+  describe('#fetch', function () {
+
+    beforeEach(function () {
+      sinon.stub(needle, 'getAsync').resolves({
+        statusCode: 200,
+        body: []
+      });
+    });
+
+    afterEach(function () {
+      needle.getAsync.restore();
+    });
+
+    var Model;
+    beforeEach(function () {
+      Model = function () {
+        ModelBase.apply(this, arguments);
+      };
+      Model.prototype = Object.create(ModelBase.prototype);
+      Model.prototype.url = function () {
+        return 'http://url';
+      };
+      collection.model = Model;
+    });
+
+    it('fetches the base URL if no attributes are defined', function () {
+      return collection.fetch().finally(function () {
+        expect(needle.getAsync).to.have.been.calledWith('http://url');
+      });
+    });
+
+    it('adds attributes to the query string', function () {
+      collection.attributes = {
+        foo: 'bar',
+        baz: 'qux'
+      };
+      return collection.fetch().finally(function () {
+        expect(needle.getAsync).to.have.been.calledWith('http://url?foo=bar&baz=qux');
+      });
+    });
+
+    it('updates existing models in the collection in place by ID', function () {
+      var model = new Model({
+        id: 1
+      });
+      collection.push(model);
+      needle.getAsync.resolves({
+        statusCode: 200,
+        body: [{
+          id: 1,
+          foo: 'bar'
+        }]
+      });
+      return collection.fetch().finally(function () {
+        expect(collection).to.have.length(1);
+        expect(collection[0])
+          .to.equal(model)
+          .and.to.have.property('foo', 'bar');
+      });
+    });
+
+    it('adds new models to the collection', function () {
+      needle.getAsync.resolves({
+        statusCode: 200,
+        body: [{
+          id: 1,
+        }]
+      });
+      return collection.fetch().finally(function () {
+        expect(collection).to.have.length(1);
+        expect(collection[0])
+          .to.be.an.instanceOf(Model)
+          .and.to.have.property('id', 1);
+      });
+    });
+
+    it('uses the model dataProperty', function () {
+      Model.prototype.dataProperty = 'data';
+      needle.getAsync.resolves({
+        statusCode: 200,
+        body: {
+          data: [{
+            id: 1,
+          }]
+        }
+      });
+      return collection.fetch().finally(function () {
+        expect(collection).to.have.length(1);
+      });
     });
 
   });

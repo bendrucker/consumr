4c8904e8cdc373623ca40fb1564a2135f35b9ab5
Ben Drucker <bvdrucker@gmail.com>
2014-04-07 15:41:59 -0400

    Simplify collection merging logic and delegate matching of new data to existing models to Model

--- src/collection.js
+++ src/collection.js
@@ -6,10 +6,22 @@ var emitThen     = require('emit-then');
 
 var internals = {};
 
-var Collection = function (attributes) {
+var Collection = function (Model, attributes) {
   Array.call(this);
   EventEmitter.call(this);
-  this.attributes = attributes || {};
+
+  Object.defineProperties(this, {
+    model: {
+      value: Model,
+      enumerable: false,
+      writable: true
+    },
+    attributes: {
+      value: attributes || {},
+      enumerable: false,
+      writable: true
+    }
+  });
 };
 
 Collection.prototype = Object.create(Array.prototype);
@@ -25,12 +37,14 @@ Collection.prototype.reset = function () {
   return this;
 };
 
-internals.cast = function (attributes) {
-  return new this(attributes);
+internals.cast = function (Model, attributes) {
+  return new Model(attributes);
 };
 
-internals.find = function (model) {
-  return _.find(this, {id: model.id});
+internals.find = function (collection, modelData) {
+  return _.find(collection, function (model) {
+    return model.matches(modelData);
+  });
 };
 
 internals.update = function (model) {
@@ -42,10 +56,16 @@ Collection.prototype.merge = function (models) {
   if (!Array.isArray(models)) models = [models];
   models
     .filter(function (model) {
-      return !internals.update.call(this, model);
+      var existing = internals.find(this, model);
+      if (existing) {
+        existing.set(model);
+        return;
+      } else {
+        return true;
+      }
     }, this)
     .forEach(function (model) {
-      this.push(internals.cast.call(this.model, model));
+      this.push(internals.cast(this.model, model));
     }, this);
 
   return this;
